56cea0816f16060f83036563d488cba6c237869e
cypherjones <ajjonez@icloud.com>
2018-02-14 17:48:54 -0600

    update http protocols

--- partials/main/head.php
+++ partials/main/head.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700|Playfair+Display" rel="stylesheet">
   <!-- <link rel="stylesheet" href="<?php bloginfo('template_directory' ); ?>/css/owl.carousel.css"> -->
   <!-- <link rel="stylesheet" href="<?php bloginfo('template_directory' ); ?>/css/owl.theme.default.min.css"> -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" defer></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" defer></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory' ); ?>/js/jquery-1.12.4.min.js"><\/script>')</script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script src="<?php bloginfo('template_directory' ); ?>/js/jquery.main.js" defer></script>
